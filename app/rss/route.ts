6e09f58ea623a285dd6473a60b6df3715ed86c87
jph-sw <jph.sw@proton.me>
2024-04-28 21:24:38 +0200

    jph-sw-fix-rss-bug

--- app/rss/route.ts
+++ app/rss/route.ts
@@ -15,7 +15,7 @@ export async function GET() {
       (post) =>
         `<item>
           <title>${post.metadata.title}</title>
-          <link>${baseUrl}/blog/${post.slug}</link>
+          <link>${baseUrl}/lernzettel/${post.slug}</link>
           <description>${post.metadata.summary || ""}</description>
           <pubDate>${new Date(
             post.metadata.publishedAt
