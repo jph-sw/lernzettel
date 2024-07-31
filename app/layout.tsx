2bfce483e6bc7752420ed51c8e391f29ad14a0aa
jph-bsw <jph@patorg.de>
2024-07-31 15:47:48 +0200

    add: favicon - apple-icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export const metadata: Metadata = {
   },
 };
 
-const cx = (...classes) => classes.filter(Boolean).join(" ");
+export const cx = (...classes) => classes.filter(Boolean).join(" ");
 
 export default function RootLayout({
   children,
@@ -49,7 +49,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={cx("text-black bg-white dark:text-white dark:bg-black", mono.className)}>
+    <html
+      lang="en"
+      className={cx(
+        "text-black bg-white dark:text-white dark:bg-black",
+        GeistMono.className
+      )}
+    >
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
 import { BlogPosts } from "app/components/posts";
+import { Gilda_Display } from "next/font/google";
+import { cx } from "./layout";
+
+const gilda = Gilda_Display({ weight: "400", subsets: ["latin"] });
 
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+      <h1
+        className={cx(
+          "mb-8 text-4xl font-semibold tracking-tighter",
+          gilda.className
+        )}
+      >
         WIT-23-1 Lernzettel
       </h1>
-      <p>Diese Seite ist eine Sammlung von den Lernzetteln der Klasse WIT-23</p>
+      <p className={cx("text-xl", gilda.className)}>
+        Diese Seite ist eine Sammlung von den Lernzetteln der Klasse WIT-23
+      </p>
 
       <div className="my-8">
         <BlogPosts />
