aad5a301707ce975e348a8c43aeb3c62f31a06c0
jph-sw <jph.sw@proton.me>
2025-09-30 11:40:10 +0000

    cloudflare

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
+
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
